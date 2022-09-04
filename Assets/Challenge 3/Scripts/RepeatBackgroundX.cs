ccd2c44a7fcaca6dc48bbe4d88e4b6e930550088
feirie001@gmail.com <feirie001@gmail.com>
2022-09-04 19:16:11 +0100

    Add project files.

---
+++ Assets/Challenge 3/Scripts/RepeatBackgroundX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackgroundX : MonoBehaviour
+{
+    //function to loop a background or frame 
+    private Vector3 startPos;
+    private float repeatWidth;
+    void Start()
+    {
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    void Update()
+    {
+        if (transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos;
+        }
+    }
+
+
+}
+
+
